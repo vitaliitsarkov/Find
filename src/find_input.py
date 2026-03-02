05ad7b8b1acc95ec3fb96682bc6991d7422fec00
vitaliitsarkov <vitaliy.tsarkov@gmail.com>
2026-03-02 18:56:46 +0300

    feat:

--- src/find_input.py
+++ src/find_input.py
@@ -3,5 +3,5 @@ import pyinputplus as pyip
 class FindInput:
     @staticmethod
     def main():
-        response = pyip.inputStr(prompt="Введите название песни: ")
+        response = pyip.inputStr(prompt="Введите песню (например, Марк Котляр путь к нашим сердцам: ")
         return response
