f3521421cdd2406de367d4b34cb850002b5da8e7
vitaliitsarkov <vitaliy.tsarkov@gmail.com>
2026-03-02 18:53:24 +0300

    feat:

--- src/find_input.py
+++ src/find_input.py
@@ -3,5 +3,5 @@ import pyinputplus as pyip
 class FindInput:
     @staticmethod
     def main():
-        response = 'Отшумели летние дожди'#pyip.inputStr(prompt="Введите название песни: ")
+        response = pyip.inputStr(prompt="Введите название песни: ")
         return response
